3db106398f9734b284a46bcbc681c57b4566f70e
janakk06 <66517079+janakk06@users.noreply.github.com>
2020-07-16 16:56:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    ground = new Ground(600,height,1200,20);
+pig1 = new Pig(800,330);
+log1 = new Log(800,250,300,PI/2);
+bird1 = new Bird(50,50);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+pig1.display();
+log1.display();
+bird1.display();
+}
